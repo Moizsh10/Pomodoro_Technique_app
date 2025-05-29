8c6b859a10be829f4db09426a42d53bbd1abfd30
Moiz Shah <moiz-shah@hotmail.com>
2025-05-29 15:01:46 -0500

    Completed method to track which part of pomodoro session is next. Fixed error that wouldn't allow Start button to re-enable

--- app/src/main/java/com/example/pomodoro_technique_app/MainActivity.java
+++ app/src/main/java/com/example/pomodoro_technique_app/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private int[] timerInvervals = new int[dataPoints];
     final int[] trackSessions = {4, 4, 1};
     private int[] numSessions = new int[3];
-    private int whichTime;
+    private int whichIndex;
     private boolean workSession = true;
     private boolean shortBreak = false;
 
@@ -45,14 +45,15 @@ public class MainActivity extends AppCompatActivity {
         Timer timer = new Timer();
         Button pauseButton = findViewById(R.id.pauseButton);
         Button resetButton = findViewById(R.id.resetButton);
-        whichTime = sessionTracker();
-        Log.d(TAG,"Index: "+whichTime+" chosen");
+        whichIndex = sessionTracker();
+        Log.d(TAG,"Index: "+ whichIndex +" chosen");
 
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pausedTimeVal = pauseTimer(v, timer);
+                pausedTimeVal = pauseTimer(timer);
                 timerPaused = true;
+
                 Log.d(TAG, "Paused at Minutes: " + pausedTimeVal[0] + " Seconds: " + pausedTimeVal[1]);
             }
         });
@@ -60,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                resetTimer(v, timer);
+                resetTimer(timer);
                 timerReset = true;
             }
         });
@@ -75,21 +76,22 @@ public class MainActivity extends AppCompatActivity {
             int minutes = Integer.parseInt(pausedTimeVal[0]) * 60000;
             int seconds = Integer.parseInt(pausedTimeVal[1]) * 1000; // Multiply by 1000 to convert seconds to milliseconds
             postPauseTime = minutes+seconds;
-            startTimer(v,timer,postPauseTime);
+            startTimer(timer,postPauseTime);
             timerPaused = false;
+            Log.d(TAG, "Starting from a pause");
         }
         else {
-            startTimer(v, timer, timerInvervals[whichTime]);
+            startTimer(timer, timerInvervals[whichIndex]);
+            Log.d(TAG, "Starting from time data array");
         }
     }
 
-    public void startTimer(View start, Timer timer, int timeLen) {
+    public void startTimer(Timer timer, int timeLen) {
 
         Log.d(TAG,"Start Button clicked");
         // Disable start button
         Button startButton = findViewById(R.id.startButton);
         startButton.setEnabled(false);
-//        startButton.setVisibility(View.INVISIBLE);
 
         TextView displayTime = findViewById(R.id.timerView); // Grab the TextView being used to display time in the app
 
@@ -116,20 +118,32 @@ public class MainActivity extends AppCompatActivity {
 
                count = count - 1000; // reduce time by one second
                if (count < 0) {
-                   displayTime.setText("Timer Finished!");
-                   startButton.setEnabled(true);
-                   timer.cancel();
-                   Log.d(TAG,"Timer Finished");
+                   try {
+                       displayTime.setText("Timer Finished!");
+                       timer.cancel();
+                       runOnUiThread(new Runnable() {
+
+                           @Override
+                           public void run() {
+
+                               startButton.setEnabled(true);
+
+                           }
+                       });
+
+                       Log.d(TAG, "Timer Finished");
+                   } catch (Exception e) {
+                       Log.d(TAG,e.getMessage());
+                   }
                }
            }
        };
        timer.schedule(task, 0, 1000); // task starts immediately, with pause of 1 second between run cycles
-
         timerReset = false;
 
     }
 
-    public String[] pauseTimer(View pause, Timer timer) {
+    public String[] pauseTimer(Timer timer) {
         String[] timeVals;
 
         Log.d(TAG,"Pause Button clicked");
@@ -146,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
 
         return timeVals;
     }
-    public void resetTimer(View reset, Timer timer) {
+    public void resetTimer(Timer timer) {
         Log.d(TAG,"Reset Button clicked");
         timer.cancel();
 
@@ -166,29 +180,43 @@ public class MainActivity extends AppCompatActivity {
 
     public int sessionTracker() {
         int index = 0;
+        TextView sessionIndicator = findViewById(R.id.sessionView);
         if (numSessions[0] == trackSessions[0] && numSessions[1] == trackSessions[1] && numSessions[2] == trackSessions[2]) {
             workSession = true;
             shortBreak = false;
             for (int i = 0; i < 3; i++) {
                 numSessions[i] = 0;
             }
-            index = 0;
-        } else if(numSessions[0] == trackSessions[0] && numSessions[1] == trackSessions[1]){
-            workSession = false;
-            shortBreak = true;
-            numSessions[2]++;
-            index = 2;
+            // immediately start a work session again, set index equal to one so the next run is a short break
+            numSessions[0] = 1;
+            sessionIndicator.setText("Work Session" + numSessions[0]);
+            Log.d(TAG,"Reset Session");
 
-        } else if (workSession && numSessions[0] < trackSessions[0]) {
+            index = 1;
+
+        } else if(numSessions[0] == trackSessions[0] && numSessions[1] == trackSessions[1]){
             workSession = true;
             shortBreak = false;
+            numSessions[2]++;
+            index = 2;
+            sessionIndicator.setText("Long Break");
+            Log.d(TAG,"Begin long break");
+
+        } else if (workSession && numSessions[0] < trackSessions[0]) {
+            workSession = false;
+            shortBreak = true;
             numSessions[0]++;
             index = 0;
+            sessionIndicator.setText("Work Session " + numSessions[0]);
+            Log.d(TAG,"Begin work session");
+
         } else if (shortBreak && numSessions[1] < trackSessions[1]){
             workSession = true;
             shortBreak = false;
             numSessions[1]++;
             index = 1;
+            sessionIndicator.setText("Short Break " + numSessions[1]);
+            Log.d(TAG,"Begin short break");
         }
 
         return index;
