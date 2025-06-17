9e119902cf94b9bbd345954c4ccc005148b24a4f
Moiz Shah <moiz-shah@hotmail.com>
2025-06-17 10:41:09 -0500

    Established layout of settings page

--- app/src/main/java/com/example/pomodoro_technique_app/CustomSettingsActivity.java
+++ app/src/main/java/com/example/pomodoro_technique_app/CustomSettingsActivity.java
@@ -1,10 +1,11 @@
 package com.example.pomodoro_technique_app;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class CustomSettingsActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
