5eb51216d347c6ed88c099e7d4acf2c4a46847bc
Moiz <moiz-shah@hotmail.com>
2026-01-12 19:54:34 -0600

    created kotlin retrieve data class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -29,6 +30,9 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
@@ -38,7 +42,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.navigation:navigation-fragment:2.8.9")
     implementation("androidx.navigation:navigation-ui:2.8.9")
+    implementation("androidx.core:core-ktx:1.17.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation("net.peanuuutz.tomlkt:tomlkt:0.5.0")
 }
---
+++ app/src/main/java/com/example/pomodoro_technique_app/RetrieveDataTOML.kt
@@ -0,0 +1,11 @@
+package com.example.pomodoro_technique_app
+
+public class RetrieveDataTOML {
+    Context context
+
+    public RetrieveDataTOML( Context nContext) {
+        this.context = nContext
+    }
+
+}
+
